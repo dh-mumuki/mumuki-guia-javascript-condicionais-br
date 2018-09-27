b6d37e9c0fd08742c3f410060b3f78200a544f96
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-27 19:00:09 -0400

    Mumuki Export on 2018-09-27 19:00:09 -0400

--- 00007_Posso ir ao banco_/test..js
+++ 00007_Posso ir ao banco_/test..js
@@ -1,5 +1,20 @@
 describe("", function() {
-  it("", function() {
-    assert(true)
-  })
+  it("possoIrAoBanco('sexta-feira', 10)", function() {
+    assert.equal(puedoIrAlBanco("sexta-feira", 10), true)
+  });
+   it("possoIrAoBanco('terça-feira', 18)", function() {
+    assert.equal(puedoIrAlBanco("terça-feira", 18), false)
+  });
+   it("possoIrAoBanco('sábado', 11)", function() {
+    assert.equal(puedoIrAlBanco("sábado", 11), false)
+  });
+  it("possoIrAoBanco('sábado', 12)", function() {
+    assert.equal(puedoIrAlBanco("sábado", 11), false)
+  });
+  it("possoIrAoBanco('sábado', 13)", function() {
+    assert.equal(puedoIrAlBanco("sábado", 11), false)
+  });
+   it("possoIrAoBanco('sábado', 14)", function() {
+    assert.equal(puedoIrAlBanco("sábado", 11), false)
+  });
 })
