d56682facdcb4ed7de1dfcbace91d0677d1cb8af
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-29 17:09:51 -0400

    Mumuki Export on 2018-09-29 17:09:51 -0400

--- 00010_A família do bom dia/test..js
+++ 00010_A família do bom dia/test..js
@@ -1,5 +1,15 @@
-describe("Descrição do grupo", function() {
-  it("Descrição da amostra", function() {
-    assert(true)
-  })
+it("arcadio y aurelianoJose son medios hermanos", function() {
+  assert.equal(sonMediosHermanos(arcadio, aurelianoJose), true);
+})
+
+it("aurelianoSegundo y remedios no son medios hermanos", function() {
+  assert.equal(sonMediosHermanos(aurelianoSegundo, remedios), false);
+})
+
+it("aurelianoSegundo y aurelianoJose no son medios hermanos", function() {
+  assert.equal(sonMediosHermanos(aurelianoSegundo, aurelianoJose), false);
+})
+
+it("remedios y arcadio no son medios hermanos", function() {
+  assert.equal(sonMediosHermanos(remedios, arcadio), false);
 })
